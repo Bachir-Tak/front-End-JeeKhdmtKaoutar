59d1e7e3c8540b9c5c0722c392c7b4bfc030709d
Bachir-Tak <Takeshi-Caw@outlook.fr>
2023-03-27 00:27:25 +0000

    First_Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemandeCreateComponent } from './view/demande/demande-create/demande-create.component';
+import { DemandeListComponent } from './view/demande/demande-list/demande-list.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemandeCreateComponent,
+    DemandeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/controller/model/demande/demande.model.ts
@@ -0,0 +1,11 @@
+import {Client} from "../client/client.model";
+import {DemandeProduit} from "../demandeProduit/demande-produit.model";
+export class Demande {
+  public id !: number;
+
+  public libelle !: string;
+
+  public code !: string;
+  public  client !: Client;
+  public demandeProduits !: DemandeProduit;
+}
