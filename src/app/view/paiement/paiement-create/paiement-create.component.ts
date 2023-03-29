0aa4c113b680b5c25f0d6adc4a0405b9c1428516
ORIGINAL SHOP <arkonteoumnia@gmail.com>
2023-03-29 16:34:28 +0000

    first commit

---
+++ src/app/view/paiement/paiement-create/paiement-create.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Paiement} from "src/app/controller/model/paiement/paiement.model";
+import {PaiementService} from "src/app/controller/service/paiement/paiement.service";
+
+@Component({
+  selector: 'app-paiement-create',
+  templateUrl: './paiement-create.component.html',
+  styleUrls: ['./paiement-create.component.css']
+})
+export class PaiementCreateComponent implements OnInit{
+  ngOnInit(): void {
+  }
+
+  public save(): void{
+    this.paiementService.save(this.paiement).subscribe(
+      data => {
+        if (data == null) {
+          alert('failure : code exist')
+        }else{
+          alert('success : paiement sav')
+        }
+      }
+    );
+  }
+
+  constructor(private paiementService: PaiementService) {
+  }
+
+
+
+  get paiement(): Paiement {
+    return this.paiementService.paiement;
+  }
+
+  set paiement(value: Paiement) {
+    this.paiementService.paiement = value;
+  }
+
+  get paiements(): Array<Paiement> {
+    return this.paiementService.paiements;
+  }
+
+  set paiements(value: Array<Paiement>) {
+    this.paiementService.paiements = value;
+  }
+
+}
